c543e79174f38f62e8137291b78f32b80ab85a3b
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-11-22 18:31:06 +0000

    Added the host home page and the account pages

---
+++ src/pages/host/AccountHost.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import NavbarHost from "../../components/host/NavbarHost";
+import AccountImage from "../../components/user/AccountImage";
+import Footer from "../../components/user/Footer";
+
+const AccountHost: React.FC = () => {
+  return (
+    <>
+      <NavbarHost />
+      <div className='flex flex-col lg:flex-row max-lg:mt-16 p-4 md:p-16 gap-8'>
+            <div className='w-full lg:hidden flex flex-col gap-6'>
+                <div className='flex flex-col gap-0 text-header-600'>
+                    <h1 className='text-2xl font-semibold'>Hello, John Doe</h1>
+                    <p className='text-sm'>Joined on Jan 2023</p>
+                </div>
+                <button className='border w-fit border-blue-400 py-2 px-5 rounded-md text-blue-400'>Edit Profile</button>
+                <div>
+                    <p>0 Reviews</p>
+                </div>
+            </div>
+            <AccountImage />
+            <div className='w-full max-lg:hidden flex flex-col gap-6'>
+                <div className='flex flex-col gap-0 text-header-600'>
+                    <h1 className='text-2xl font-semibold'>Hello, John Doe</h1>
+                    <p className='text-sm'>Joined on Jan 2023</p>
+                </div>
+                <button className='border w-fit border-blue-400 py-2 px-5 rounded-md text-blue-400'>Edit Profile</button>
+                <div>
+                    <p>0 Reviews</p>
+                </div>
+            </div>
+        <div className="text-header-600 bg-blue-200 p-5 rounded-md w-full flex gap-2 flex-col">
+          <h1 className="font-semibold text-xl">Personal Details</h1>
+          <div>
+            <h1 className="text-lg font-semibold">Name</h1>
+            <p>John Doe</p>
+          </div>
+          <div>
+            <h1 className="text-lg font-semibold">Email</h1>
+            <p>johndoe@gmail.com</p>
+          </div>
+          <div>
+            <h1 className="text-lg font-semibold">About</h1>
+            <p>
+              Hi, I’m James, a digital marketer who loves exploring new places
+              and cultures. I enjoy finding unique, comfortable stays for both
+              short trips and extended stays.
+            </p>
+          </div>
+          <div>
+            <h1 className="text-lg font-semibold">Phone Number</h1>
+            <p>+233 123456789</p>
+          </div>
+          <div>
+            <h1 className="text-lg font-semibold">Work</h1>
+            <p>Traveler</p>
+          </div>
+          <div>
+            <h1 className="text-lg font-semibold">Location</h1>
+            <p>Accra, Ghana</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AccountHost
---
+++ src/pages/host/PropertiesHost.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import NavbarHost from "../../components/host/NavbarHost";
+import Footer from "../../components/user/Footer";
+
+
+const PropertiesHost: React.FC = () => {
+    return (
+        <div>
+            <NavbarHost />
+            <div className='p-16'>
+                <div className="flex items-center justify-between">
+                    <h1 className="text-4xl font-bold text-header-600">List Properties</h1>
+                    <div className="bg-blue-600 rounded-3xl py-2 px-4 text-white cursor-pointer">Add Property</div>
+                </div>
+                <div role="tablist" className="tabs tabs-bordered py-6">
+                    <input type="radio" name="my_tabs_1" role="tab" className="tab text-lg font-semibold" aria-label="Approved" defaultChecked />
+                    <div role="tabpanel" className="tab-content p-10">
+                        {/* Approved Content */}
+                    </div>
+                    
+                    <input type="radio" name="my_tabs_1" role="tab" className="tab text-lg font-semibold" aria-label="Pending"/>
+                    <div role="tabpanel" className="tab-content p-10">
+                        {/* Pending Content */}
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default PropertiesHost
